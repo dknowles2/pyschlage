7ed7915849f6df0544da313358103e2d79ab661e
David Knowles <dknowles2@gmail.com>
2022-12-19 16:36:07 -0500

    s/device/lock/g in test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -8,70 +8,70 @@ from .fixtures import LOCK_JSON
 
 def test_from_json():
     auth = mock.Mock()
-    device = pyschlage.Lock.from_json(auth, LOCK_JSON)
-    assert device._auth == auth
-    assert device.device_id == "__device_uuid__"
-    assert device.name == "Door Lock"
-    assert device.model_name == "__model_name__"
-    assert device.battery_level == 95
-    assert device.is_locked
-    assert not device.is_jammed
-    assert device.firmware_version == "10.00.00264232"
+    lock = pyschlage.Lock.from_json(auth, LOCK_JSON)
+    assert lock._auth == auth
+    assert lock.device_id == "__device_uuid__"
+    assert lock.name == "Door Lock"
+    assert lock.model_name == "__model_name__"
+    assert lock.battery_level == 95
+    assert lock.is_locked
+    assert not lock.is_jammed
+    assert lock.firmware_version == "10.00.00264232"
 
 
 def test_from_json_is_jammed():
     auth = mock.Mock()
     json = deepcopy(LOCK_JSON)
     json["attributes"]["lockState"] = 2
-    device = pyschlage.Lock.from_json(auth, json)
-    assert not device.is_locked
-    assert device.is_jammed
+    lock = pyschlage.Lock.from_json(auth, json)
+    assert not lock.is_locked
+    assert lock.is_jammed
 
 
 def test_update():
     auth = mock.Mock()
-    device = pyschlage.Lock.from_json(auth, LOCK_JSON)
+    lock = pyschlage.Lock.from_json(auth, LOCK_JSON)
     new_json = deepcopy(LOCK_JSON)
     new_json["name"] = "<NAME>"
 
     auth.request.return_value = mock.Mock(json=mock.Mock(return_value=new_json))
-    device.update()
+    lock.update()
 
     auth.request.assert_called_once_with("get", "devices/__device_uuid__")
-    assert device.name == "<NAME>"
+    assert lock.name == "<NAME>"
 
 
 def test_lock():
     auth = mock.Mock()
     initial_json = deepcopy(LOCK_JSON)
     initial_json["attributes"]["lockState"] = 0
-    device = pyschlage.Lock.from_json(auth, LOCK_JSON)
+    lock = pyschlage.Lock.from_json(auth, LOCK_JSON)
 
     new_json = deepcopy(LOCK_JSON)
     new_json["attributes"]["lockState"] = 1
 
     auth.request.return_value = mock.Mock(json=mock.Mock(return_value=new_json))
-    device.lock()
+    lock.lock()
 
     auth.request.assert_called_once_with(
         "put", "devices/__device_uuid__", json={"attributes": {"lockState": 1}}
     )
-    assert device.is_locked
+    assert lock.is_locked
 
 
 def test_unlock():
     auth = mock.Mock()
     initial_json = deepcopy(LOCK_JSON)
     initial_json["attributes"]["lockState"] = 1
-    device = pyschlage.Lock.from_json(auth, LOCK_JSON)
+    lock = pyschlage.Lock.from_json(auth, LOCK_JSON)
 
     new_json = deepcopy(LOCK_JSON)
     new_json["attributes"]["lockState"] = 0
 
     auth.request.return_value = mock.Mock(json=mock.Mock(return_value=new_json))
-    device.unlock()
+    lock.unlock()
 
     auth.request.assert_called_once_with(
         "put", "devices/__device_uuid__", json={"attributes": {"lockState": 0}}
     )
-    assert not device.is_locked
+    assert not lock.is_locked
